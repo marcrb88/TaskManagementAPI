0abd04a4fe092d286b6d1a200f8d605710d04897
Marc Roige Benaiges <marcroige88@gmail.com>
2025-09-27 12:10:18 +0200

    domain-model: Add User domain entity

---
+++ src/Domain/Model/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Domain\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use DateTime;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid")
+     */
+    private string $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $name;
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private string $email;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTime $createdAt;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
