4b8b6c2d7a2e579f671b30169b42f10b3b915893
marcrb88 <marcroige88@gmail.com>
2025-10-04 21:04:22 +0200

    date-format-validator: Fix date format validator

--- src/Application/Service/DateFormatValidator.php
+++ src/Application/Service/DateFormatValidator.php
@@ -9,9 +9,10 @@ class DateFormatValidator
     public function validate(array $datesToValidate): bool
     {
         foreach ($datesToValidate as $dateField) {
-            $date = DateTime::createFromFormat('Y-m-d\TH:i:s', $dateField);
-            if ($date === false) {
-                return false;
+            try {
+                new DateTime($dateField);
+            } catch (\Exception $e) {
+                return false; 
             }
         }
         return true;
